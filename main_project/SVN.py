c5c75d3c75b8ae40cbad3c855c12b839d9990414
CiocanAlexandru <alexciocan2001@gmail.com>
2023-12-07 02:09:52 +0200

    Part7 Still stuck in svm implementatio gridShearc almost ready MFCC problem

--- main_project/SVN.py
+++ main_project/SVN.py
@@ -1,4 +1,5 @@
-import common_library
+import common_library 
+common_library.warnings.filterwarnings("ignore")
 class SVN:
     def __init__(self,class_index,encoded_data=None,diagrams=None,features_name=None):
         self.encoded_data=encoded_data
@@ -11,12 +12,33 @@ class SVN:
         print("SVN model initialiezed ")
         self.features_name=features_name
         self.name="SVN"
-        best_setings=self.GridShearch()
-        self.C=None
-        self.Gamma=None
-        self.Kernel=None
+        self.GridShearch()
+        #self.best_settings=self.GridShearch()
+        #self.C=None
+        #self.Gamma=None
+        #self.Kernel='2'
     def GridShearch(self):
-        return 0
+        if self.features[0].shape[0]==40:
+           #MFCC
+           
+           print("Best Hyperparameters:", best_params)
+        else:
+           #FFT or PSD
+           ##Inpartire set de date
+           X_train, X_test, y_train, y_test=common_library.train_test_split(self.features,self.transformed_labels,test_size=3)
+           svm_model = common_library.OneVsRestClassifier(common_library.SVC())
+           param_grid = {
+           'estimator__C': [0.1, 1, 10, 100],         # Regularization parameter
+           'estimator__kernel': ['linear', 'rbf'],   # Kernel type
+           'estimator__gamma': [1, 0.1, 0.01, 0.001, 0.0001] # Kernel coefficient for 'rbf' kernel
+           }
+           X_train=X_train[0:500]
+           y_train=y_train[0:500]
+           grid_search = common_library.GridSearchCV(svm_model, param_grid, cv=2, scoring='accuracy', n_jobs=-1)
+           grid_search.fit(X_train, y_train)
+           best_params = grid_search.best_params_
+           print("Best Hyperparameters:", best_params)
+    
     def Model(self):
         return 0
     def Training(self):
--- main_project/common_library.py
+++ main_project/common_library.py
@@ -8,11 +8,15 @@ import seaborn as sns
 import tensorflow as tf
 import matplotlib.pyplot as plt
 import random 
+import warnings
+from libsvm.svmutil import *
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.metrics import make_scorer
 from scipy.io.wavfile import read
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MultiLabelBinarizer
 from sklearn.metrics import accuracy_score
-from sklearn.model_selection import StratifiedKFold
+from sklearn.model_selection import StratifiedKFold, GridSearchCV
 from sklearn.model_selection import KFold
 from datetime import datetime
 from sklearn.svm import SVC
