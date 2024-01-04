a561cb899ca16b1e591c501f01b2cdb5bf1d0409
CiocanAlexandru <79212626+CiocanAlexandru@users.noreply.github.com>
2024-01-04 11:18:16 +0200

    Update routing.php

--- Web_Interface/routing/routing.php
+++ Web_Interface/routing/routing.php
@@ -1,5 +1,31 @@
 <?php
-
+/*
+function CheckURL($Features,$Project_Name,$arr)
+{
+    $poz1=0;$poz2=0;$coun=0;$k=0;
+    foreach($arr as $i)
+    {
+        if ($i===$Project_Name)
+        {
+            $count=$count+1;
+            $poz1=$k;
+        }
+        if ($i===$Features)
+        {
+            $count=$count-1;
+            $poz2=$k;
+        }  
+        if ($i!=$Features && $i!=$Project_Name && $i!="localhost")
+          echo "Difreant";
+        $k=$k+1;  
+    }
+    if ($count!=0)
+      return false;
+    if ($poz1>$poz2)
+      return false;
+    return true;
+};
+*/
 $arr= explode("/", $_SERVER['REQUEST_URI'] );
 
 if (in_array("Home", $arr))
