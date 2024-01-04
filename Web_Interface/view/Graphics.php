e99cbead87a84159f0c0c5b50291faa8cce46293
CiocanAlexandru <alexciocan2001@gmail.com>
2024-01-04 09:31:26 +0200

    Frontend Done need just some check up

--- Web_Interface/view/Graphics.php
+++ Web_Interface/view/Graphics.php
@@ -28,22 +28,46 @@
     <section>
         <div class="content">
         <h1>Graphics</h1>
-        <p>Some content</p>
-        <p>Some content</p>
-        <p>Some content</p>
-        <p>Some content</p>
-        <p>Some content</p>
-        <p>Some content</p>
-        <p>Some content</p>
-        <p>Some content</p>
-        <p>Some content</p>
-        <p>Some content</p>
-        <p>Some content</p>
-        <p>Some content</p>
-        <p>Some content</p>
-        <p>Some content</p>
-        <p>Some content</p>
-        <p>Some content</p>
+            <p><strong>Let's see some graphics performens</strong></p>
+            <div class="graphics" accept=".wav">
+            <img src="../images/Fundal Image.jpeg">
+            </div>
+            <form action="" method="Post">
+            <div class="options">
+            <label for="model">Model</label>
+            <select id="model" name="model">
+            <option value="FCNN">FCNN</option>
+            <option value="CNN">CNN</option>
+            <option value="SVM">SVM</option>
+            </select>
+            
+            <label for="Fueatures">Fueatures</label>
+            <select id="Fueatures" name="Features">
+            <option value="FFT">FFT</option>
+            <option value="MFCC">MFCC</option>
+            <option value="PSD">PSD</option>
+            </select>
+
+            <label for="Train">Traing Method</label>
+            <select id="Train" name="Train">
+            <option value="Normal">Normal</option>
+            <option value="Nkfold">Nkfold</option>
+            </select>
+
+            <label for="Lost-Function">Lost-Function</label>
+            <select id="Lost-Function" name="Lost-Function">
+            <option value="One-Function">One function</option>
+            <option value="Multi-Function">Multi Function</option>
+            </select>
+            <label for="Number of cycel">Cycel nkfold</label>
+            <select id="Number of cycel" name="Number of cycel">
+            <option value="One-Function">One cycel</option>
+            <option value="Multi-Function">More cycles 3 5 7 11</option>
+            </select>
+            <button type="submit" class="submit-button">Submit</button>
+            </div>
+            </form>
+           
         </div>
     </section>
     <footer>
--- Web_Interface/view/Prediction.php
+++ Web_Interface/view/Prediction.php
@@ -26,24 +26,51 @@
         </ul>
     </div>
     <section>
+        
         <div class="content">
-        <h1>Prediction</h1>
-        <p>Some content</p>
-        <p>Some content</p>
-        <p>Some content</p>
-        <p>Some content</p>
-        <p>Some content</p>
-        <p>Some content</p>
-        <p>Some content</p>
-        <p>Some content</p>
-        <p>Some content</p>
-        <p>Some content</p>
-        <p>Some content</p>
-        <p>Some content</p>
-        <p>Some content</p>
-        <p>Some content</p>
-        <p>Some content</p>
-        <p>Some content</p>
+            <h1>Prediction</h1>
+            <p><strong>Note :The file you upload must be wav!!</strong></p>
+            <form action="" method="Post">
+            <div class="file-upload" accept=".wav">
+            <input class="audio-file" type="file" name="fileToUpload" id="fileToUpload" accept=".txt, .pdf, .doc, .docx">
+            <label class="label-file" for="fileToUpload" data-file-name="No file chosen">Upload File</label>
+            </div>
+            <div class="options">
+            <label for="model">Model</label>
+            <select id="model" name="model">
+            <option value="FCNN">FCNN</option>
+            <option value="CNN">CNN</option>
+            <option value="SVM">SVM</option>
+            </select>
+            
+            <label for="Fueatures">Fueatures</label>
+            <select id="Fueatures" name="Features">
+            <option value="FFT">FFT</option>
+            <option value="MFCC">MFCC</option>
+            <option value="PSD">PSD</option>
+            </select>
+
+            <label for="Train">Traing Method</label>
+            <select id="Train" name="Train">
+            <option value="Normal">Normal</option>
+            <option value="Nkfold">Nkfold</option>
+            </select>
+
+            <label for="Lost-Function">Lost-Function</label>
+            <select id="Lost-Function" name="Lost-Function">
+            <option value="One-Function">One function</option>
+            <option value="Multi-Function">Multi Function</option>
+            </select>
+            <label for="Number of cycel">Cycel nkfold</label>
+            <select id="Number of cycel" name="Number of cycel">
+            <option value="One-Function">One cycel</option>
+            <option value="Multi-Function">More cycles 3 5 7 11</option>
+            </select>
+            <button type="submit" class="submit-button">Submit</button>
+            </div>
+            </form>
+           
+
         </div>
     </section>
     <footer>
